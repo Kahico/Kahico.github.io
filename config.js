9524a206a27498e1007abb08c2a95e7b69999e42
Kahico <137761026+Kahico@users.noreply.github.com>
2023-07-03 14:19:24 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -18,8 +18,5 @@ var klaroConfig = {
         }
     ],
   };
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-8Y2MNS08G7');  
+  
+  
