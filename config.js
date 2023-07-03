f86cb8804ff32660831abf2fa4ca9b5f8237885c
Kahico <137761026+Kahico@users.noreply.github.com>
2023-07-03 12:50:12 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+var klaroConfig = {
+    translations: {
+      zz: {
+        googleAnalytics: {
+          title: "Google Analytics",
+          description: "Service d'analyse d'audience du site Web.",
+        },
+        purposes: {
+          analytics: "Analytics"
+        }
+      }
+    },
+    services: [
+        {
+            name: "googleAnalytics",
+            default: true,
+            purposes: ["analytics"]
+        }
+    ],
+  };
+  
